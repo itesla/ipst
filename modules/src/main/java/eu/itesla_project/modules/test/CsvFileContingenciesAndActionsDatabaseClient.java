fb9494b0442f774d1b31aab127533d06407d720a
Giovanni Ferrari <giovanni.ferrari@quinary.com>
2017-08-10 10:14:46 +0200

    Refactoring

--- modules/src/main/java/eu/itesla_project/modules/test/CsvFileContingenciesAndActionsDatabaseClient.java
+++ modules/src/main/java/eu/itesla_project/modules/test/CsvFileContingenciesAndActionsDatabaseClient.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.UncheckedIOException;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -20,15 +21,17 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import eu.itesla_project.contingency.BranchContingency;
 import eu.itesla_project.contingency.Contingency;
 import eu.itesla_project.contingency.ContingencyElement;
 import eu.itesla_project.contingency.ContingencyImpl;
 import eu.itesla_project.contingency.GeneratorContingency;
-import eu.itesla_project.contingency.LineContingency;
 import eu.itesla_project.iidm.network.Line;
 import eu.itesla_project.iidm.network.Network;
 import eu.itesla_project.iidm.network.TieLine;
@@ -43,7 +46,9 @@ import eu.itesla_project.modules.contingencies.Zone;
 /**
  * Contingencies and actions database based on CSV file. Can only contain NmK
  * line contingencies.
- * <p>Example:
+ * <p>
+ * Example:
+ * 
  * <pre>
  *#contingency id;line count;line1 id;line2 id;...
  * ...
@@ -55,31 +60,15 @@ public class CsvFileContingenciesAndActionsDatabaseClient implements Contingenci
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CsvFileContingenciesAndActionsDatabaseClient.class);
 
-    private final InputStream input;
+    private final List<ContingencyData> condingency_data = new ArrayList();
 
     public CsvFileContingenciesAndActionsDatabaseClient(Path file) throws IOException {
-        input = new FileInputStream(file.toFile());
+        this(new FileInputStream(file.toFile()));
     }
 
-    public CsvFileContingenciesAndActionsDatabaseClient(InputStream data) {
-        this.input = data;
-    }
-
-    @Override
-    public List<Contingency> getContingencies(Network network) {
-        // pre-index tie lines
-        Map<String, String> tieLines = new HashMap<>();
-        for (Line l : network.getLines()) {
-            if (l.isTieLine()) {
-                TieLine tl = (TieLine) l;
-                tieLines.put(tl.getHalf1().getId(), tl.getId());
-                tieLines.put(tl.getHalf2().getId(), tl.getId());
-            }
-        }
-
-        List<Contingency> contingencies = new ArrayList<>();
+    public CsvFileContingenciesAndActionsDatabaseClient(InputStream input) {
         try {
-            Reader ir = new InputStreamReader(input,Charset.defaultCharset());
+            Reader ir = new InputStreamReader(input, Charset.defaultCharset());
             try (BufferedReader r = new BufferedReader(ir)) {
 
                 String txt;
@@ -95,34 +84,62 @@ public class CsvFileContingenciesAndActionsDatabaseClient implements Contingenci
                         throw new RuntimeException("Error parsing '" + txt + "'");
                     }
                     String contingencyId = tokens[0];
+                    ContingencyData cd = new ContingencyData(contingencyId);
                     int lineCount = Integer.parseInt(tokens[1]);
                     if (tokens.length != lineCount + 2) {
                         throw new RuntimeException("Error parsing '" + txt + "'");
                     }
-                    List<ContingencyElement> elements = new ArrayList<>(lineCount);
                     for (int i = 2; i < lineCount + 2; i++) {
                         String id = tokens[i];
-                        if (network.getLine(id) != null) {
-                            elements.add(new LineContingency(id));
-                        } else if (network.getGenerator(id) != null) {
-                            elements.add(new GeneratorContingency(id));
-                        } else if (tieLines.containsKey(id)) {
-                            elements.add(new LineContingency(tieLines.get(id)));
-                        } else {
-                            LOGGER.warn("Contingency element '{}' not found", id);
-                        }
+                        cd.addElementId(id);
                     }
-                    if (elements.size() > 0) {
-                        contingencies.add(new ContingencyImpl(contingencyId, elements));
-                    } else {
-                        LOGGER.warn("Skip empty contingency " + contingencyId);
-                    }
+                    condingency_data.add(cd);
                 }
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new UncheckedIOException(e);
         }
+    }
+
+    @Override
+    public List<Contingency> getContingencies(Network network) {
+        // pre-index tie lines
+        Map<String, String> tieLines = new HashMap<>();
+        for (Line l : network.getLines()) {
+            if (l.isTieLine()) {
+                TieLine tl = (TieLine) l;
+                tieLines.put(tl.getHalf1().getId(), tl.getId());
+                tieLines.put(tl.getHalf2().getId(), tl.getId());
+            }
+        }
+
+        List<Contingency> contingencies = new ArrayList<>();
+        condingency_data.forEach(cd -> {
+            List<ContingencyElement> elements = cd.getElementsIds().stream()
+                    .map(id -> getElement(network, tieLines, cd.getId()))
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+            if (elements.size() > 0) {
+                contingencies.add(new ContingencyImpl(cd.getId(), elements));
+            } else {
+                LOGGER.warn("Skip empty contingency " + cd.getId());
+            }
+        });
         return contingencies;
+    }
+
+    private ContingencyElement getElement(Network network, Map<String, String> tieLines, String id) {
+        if (network.getLine(id) != null) {
+            return new BranchContingency(id);
+        } else if (network.getGenerator(id) != null) {
+            return new GeneratorContingency(id);
+        } else if (tieLines.containsKey(id)) {
+            return new BranchContingency(tieLines.get(id));
+        } else {
+            LOGGER.warn("Contingency element '{}' not found", id);
+        }
+        return null;
+
     }
 
     @Override
@@ -130,78 +147,99 @@ public class CsvFileContingenciesAndActionsDatabaseClient implements Contingenci
         return Collections.emptyList();
     }
 
-	@Override
-	public Zone getZone(String id) {
-		 throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Collection<ActionPlan> getActionPlans() {
-		 throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public ActionPlan getActionPlan(String id) {
-		 throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Collection<Zone> getZones() {
-		 throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Collection<ActionsContingenciesAssociation> getActionsCtgAssociations() {
-		 throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Collection<ActionsContingenciesAssociation> getActionsCtgAssociationsByContingency(
-			String contingencyId) {
-		 throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Contingency getContingency(String name, Network network) {
-		 for (Contingency c : getContingencies(network)) {
-             if (c.getId().equals(name)) {
-                 return c;
-             }
-         }
-        return null;
-    }
-
-	@Override
-	public Collection<Action> getActions(Network network) {
-		 throw new UnsupportedOperationException();
-		
-	}
-
-	@Override
-	public Action getAction(String id, Network network) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Collection<Zone> getZones(Network network) {
-        throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public Collection<ActionPlan> getActionPlans(Network network) {
-        throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public List<ActionsContingenciesAssociation> getActionsCtgAssociations(
-			Network network) {
+    @Override
+    public Zone getZone(String id) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Collection<ActionsContingenciesAssociation> getActionsCtgAssociationsByConstraint(
-            String equipmentId, ConstraintType constraintType) {
+    public Collection<ActionPlan> getActionPlans() {
         throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ActionPlan getActionPlan(String id) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Collection<Zone> getZones() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Collection<ActionsContingenciesAssociation> getActionsCtgAssociations() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Collection<ActionsContingenciesAssociation> getActionsCtgAssociationsByContingency(String contingencyId) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Contingency getContingency(String name, Network network) {
+        for (Contingency c : getContingencies(network)) {
+            if (c.getId().equals(name)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Collection<Action> getActions(Network network) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    @Override
+    public Action getAction(String id, Network network) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Collection<Zone> getZones(Network network) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Collection<ActionPlan> getActionPlans(Network network) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<ActionsContingenciesAssociation> getActionsCtgAssociations(Network network) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Collection<ActionsContingenciesAssociation> getActionsCtgAssociationsByConstraint(String equipmentId,
+            ConstraintType constraintType) {
+        throw new UnsupportedOperationException();
+    }
+
+    private class ContingencyData {
+        private final String id;
+        private final List<String> elementIds;
+
+        ContingencyData(String id) {
+            this.id = id;
+            elementIds = new ArrayList();
+        }
+
+        String getId() {
+            return this.id;
+        }
+
+        void addElementId(String id) {
+            elementIds.add(id);
+        }
+
+        List<String> getElementsIds() {
+            return elementIds;
+        }
+
     }
 
 }
