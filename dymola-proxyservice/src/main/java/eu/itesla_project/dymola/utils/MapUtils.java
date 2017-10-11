ee7d6bef67d55b7c1da980a0243acada38cf0014
Christian Biasuzzi <christian.biasuzzi@quinary.com>
2017-10-11 15:41:37 +0200

    fixes style, cleans pom

--- dymola-proxyservice/src/main/java/eu/itesla_project/dymola/utils/MapUtils.java
+++ dymola-proxyservice/src/main/java/eu/itesla_project/dymola/utils/MapUtils.java
@@ -12,53 +12,55 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- *
  * @author Quinary <itesla@quinary.com>
  */
 public final class MapUtils {
- 
-  public static class Entry<K, V> {
-    private K key;
-    private V value;
- 
-    public Entry(final K key, final V value) {
-      this.key = key;
-      this.value = value;
+
+    private MapUtils() {
     }
- 
-    public K getKey() {
-      return key;
+
+    public static <K, V> Entry<K, V> entry(final K key, final V value) {
+        return new Entry(key, value);
     }
- 
-    public V getValue() {
-      return value;
+
+    public static <K, V> Map<K, V> asMap(final Entry<K, V>... entries) {
+        return populate(new HashMap<K, V>(), entries);
     }
-  }
- 
-  public static <K, V> Entry<K, V> entry(final K key, final V value) {
-    return new Entry(key, value);
-  }
- 
-  public static <K, V> Map<K, V> asMap(final Entry<K, V>... entries) {
-    return populate(new HashMap<K, V>(), entries);
-  }
- 
-  public static <K, V> Map<K, V> asOrderedMap(final Entry<K, V>... entries) {
-    return populate(new LinkedHashMap<K, V>(), entries);
-  }
- 
-  public static <K, V> Map<K, V> asUnmodifiableMap(final Entry<K, V>... entries) {
-    return Collections.unmodifiableMap(populate(new HashMap<K, V>(), entries));
-  }
- 
-  public static <K, V> Map<K, V> asUnmodifiableOrderedMap(final Entry<K, V>... entries) {
-    return Collections.unmodifiableMap(populate(new LinkedHashMap<K, V>(), entries));
-  }
- 
-  private static <K, V> Map<K, V> populate(final Map map, final Entry<K, V>... entries) {
-    for (final Entry entry : entries) {
-      map.put(entry.getKey(), entry.getValue());
+
+    public static <K, V> Map<K, V> asOrderedMap(final Entry<K, V>... entries) {
+        return populate(new LinkedHashMap<K, V>(), entries);
     }
-    return map;
-  }
+
+    public static <K, V> Map<K, V> asUnmodifiableMap(final Entry<K, V>... entries) {
+        return Collections.unmodifiableMap(populate(new HashMap<K, V>(), entries));
+    }
+
+    public static <K, V> Map<K, V> asUnmodifiableOrderedMap(final Entry<K, V>... entries) {
+        return Collections.unmodifiableMap(populate(new LinkedHashMap<K, V>(), entries));
+    }
+
+    private static <K, V> Map<K, V> populate(final Map map, final Entry<K, V>... entries) {
+        for (final Entry entry : entries) {
+            map.put(entry.getKey(), entry.getValue());
+        }
+        return map;
+    }
+
+    public static class Entry<K, V> {
+        private K key;
+        private V value;
+
+        public Entry(final K key, final V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
 }
