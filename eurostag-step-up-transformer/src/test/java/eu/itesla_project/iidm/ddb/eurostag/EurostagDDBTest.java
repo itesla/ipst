dd1574be51393ed5b520e04fcab8b45bdfe3fcfa
Boubakeur BRAHIMI <brahimibob@yahoo.fr>
2017-10-23 10:42:16 +0200

    variable renaming

--- eurostag-step-up-transformer/src/test/java/eu/itesla_project/iidm/ddb/eurostag/EurostagDDBTest.java
+++ eurostag-step-up-transformer/src/test/java/eu/itesla_project/iidm/ddb/eurostag/EurostagDDBTest.java
@@ -28,18 +28,18 @@ public class EurostagDDBTest {
         Path path4  = fs.getPath("/path1/path4");
         Path file4  = fs.getPath("/path1/path4/file4.tg");
         Path path2 = fs.getPath("/path2");
-        Path target = fs.getPath("/path2/target.tg");
+        Path file5 = fs.getPath("/path2/file5.tg");
         Path path3 = fs.getPath("/path3");
         Path p3file = fs.getPath("/path3/p3file.tg");
         Files.createDirectory(path1);
         Files.createFile(file1);
         Files.createFile(file2);
         Files.createSymbolicLink(lnpath3, path3);
-        Files.createSymbolicLink(ln, target);
+        Files.createSymbolicLink(ln, file5);
         Files.createDirectory(path4);
         Files.createFile(file4);
         Files.createDirectory(path2);
-        Files.createFile(target);
+        Files.createFile(file5);
         Files.createDirectory(path3);
         Files.createFile(p3file);
         List<Path> ddbDirs = new ArrayList<>();
@@ -49,7 +49,7 @@ public class EurostagDDBTest {
         assertEquals(eurostagDDB.findGenerator("p3file").getFileName().toString(), "p3file.tg");
         assertEquals(eurostagDDB.findGenerator("file4").getFileName().toString(), "file4.tg");
         assertEquals(eurostagDDB.findGenerator("file1").getFileName().toString(), "file1.tg");
-        assertEquals(eurostagDDB.findGenerator("target").getFileName().toString(), "target.tg");
+        assertEquals(eurostagDDB.findGenerator("file5").getFileName().toString(), "file5.tg");
         }
     }
 
@@ -57,7 +57,7 @@ public class EurostagDDBTest {
     public void testFindGenerator() throws IOException {
         try (FileSystem fs = Jimfs.newFileSystem(Configuration.unix())) {
         Path path2 = fs.getPath("/path2");
-        Path filereal = fs.getPath("/path2/target.tg");
+        Path filereal = fs.getPath("/path2/file5.tg");
         Path path = fs.getPath("lnpath2");
         Files.createDirectory(path2);
         Files.createFile(filereal);
@@ -65,7 +65,7 @@ public class EurostagDDBTest {
         List<Path> ddbDirs = new ArrayList<>();
         ddbDirs.add(path);
         EurostagDDB eurostagDDB = new EurostagDDB(ddbDirs);
-        assertEquals(eurostagDDB.findGenerator("target").getFileName().toString(), "target.tg");
+        assertEquals(eurostagDDB.findGenerator("file5").getFileName().toString(), "file5.tg");
         }
     }
 
