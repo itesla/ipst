deb470a5bed864affd464f399333cd6e97cce131
Christian Biasuzzi <christian.biasuzzi@techrain.eu>
2018-06-22 20:55:06 +0200

    py-powsybl python gateway

---
+++ py-powsybl/README.md
@@ -0,0 +1,42 @@
+#Py-powsybl:  how-to use the powsybl APIs from Python
+
+py-powsybl module uses [py4j](https://www.py4j.org) to call the Powsybl java APIs, from a Python environment.
+ 
+###Runtime requirements:
+Py4j enables Python programs running in a Python interpreter to dynamically access Java objects in a Java Virtual Machine.
+
+Py4j documentation states that it has been tested with Python 2.6, 2.7, 3.4, 3.5, and 3.6
+To install Py4j v0.10.7 on CentOS (detailed information [here](https://www.py4j.org/install.html#id1):
+    
+    pip install py4j (Python v2)
+    
+or
+    
+    pip3 install py4j (Python v3)
+ 
+Note: when it comes to experimenting with multiple Python extensions, it might be a good idea to create first an isolated python environment with [virtualenv](https://virtualenv.pypa.io/en/stable/)
+       
+###Execute py-powsybl
+
+Start the py-powsybl 'server' in a console. 
+
+    itools py-powsybl
+   
+###Run some python code:
+
+Demo1.py demostrates what can be done (it executes a loadflow on a network, opens a switch, exports a network to a file in xiidm format)
+
+    python Demo1.py     
+
+###Stop py-powsybl
+To stop the py-powsybl 'server',  CTRL+C in the itools console. 
+ 
+
+## Notes
+py-powsybl has been tested on a CentOS 6, with python v2.6, python v3.6 (in a virtualenv sandbox).
+
+## TODO
+* performances/memory with multiple, large networks/data ? 
+* make the client/server connection configurable (currently, it uses the defaults py4j)
+* explore the other way round (calling python from java)
+* issues? threads? security? 
---
+++ py-powsybl/src/main/java/com/powsybl/powsybl/PyPowsybl.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2018, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.powsybl;
+
+import com.google.auto.service.AutoService;
+import com.powsybl.tools.Command;
+import com.powsybl.tools.Tool;
+import com.powsybl.tools.ToolRunningContext;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+import py4j.GatewayServer;
+
+/**
+ * @author Christian Biasuzzi <christian.biasuzzi@techrain.it>
+ */
+@AutoService(Tool.class)
+public class PyPowsybl implements Tool {
+
+    final GatewayServer server = new GatewayServer();
+
+    @Override
+    public Command getCommand() {
+        return new Command() {
+
+            @Override
+            public String getName() {
+                return "py-powsybl";
+            }
+
+            @Override
+            public String getTheme() {
+                return "Misc";
+            }
+
+            @Override
+            public String getDescription() {
+                return "run py-powsybl gateway";
+            }
+
+            @Override
+            public Options getOptions() {
+                Options options = new Options();
+                return options;
+            }
+
+            @Override
+            public String getUsageFooter() {
+                return null;
+            }
+        };
+
+    }
+
+    @Override
+    public void run(CommandLine line, ToolRunningContext context) throws Exception {
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                System.out.println();
+                System.out.println("Shutting down py-powsybl server");
+                server.shutdown();
+            }
+        });
+
+        // execute the py4j Gateway service
+        //TODO customize service parameters: address, port, .....
+        server.start();
+        System.out.println("py-powsybl server started; CTRL+C to stop it");
+
+    }
+}
