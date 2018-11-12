083271dd70cb4728d3e63856648642f5058509ef
Christian Biasuzzi <christian.biasuzzi@techrain.eu>
2018-11-12 21:03:18 +0100

    filter out invalid contingencies in Eurostag impact analysis and export tool

--- eurostag-integration/src/main/java/eu/itesla_project/eurostag/EurostagExportTool.java
+++ eurostag-integration/src/main/java/eu/itesla_project/eurostag/EurostagExportTool.java
@@ -107,7 +107,8 @@ public class EurostagExportTool implements Tool, EurostagConstants {
         try (BufferedWriter writer = Files.newBufferedWriter(outputDir.resolve(PRE_FAULT_SEQ_FILE_NAME), StandardCharsets.UTF_8)) {
             scenario.writePreFaultSeq(writer, PRE_FAULT_SAC_FILE_NAME);
         }
-        ContingenciesProvider contingenciesProvider = defaultConfig.newFactoryImpl(ContingenciesProviderFactory.class).create();
+        ContingenciesProvider contingenciesProvider = new FilterValidContingenciesProvider(defaultConfig.newFactoryImpl(ContingenciesProviderFactory.class).create());
+
         scenario.writeFaultSeqArchive(contingenciesProvider.getContingencies(network), network, dictionary, faultNum -> FAULT_SEQ_FILE_NAME.replace(com.powsybl.computation.CommandConstants.EXECUTION_NUMBER_PATTERN, Integer.toString(faultNum)))
                 .as(ZipExporter.class).exportTo(outputDir.resolve(ALL_SCENARIOS_ZIP_FILE_NAME).toFile());
 
